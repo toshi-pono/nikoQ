36e6a154f9be2224c9d63f748f134c35003703ab
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-01-04 23:19:16 +0900

    getMessage getLogin getUserの作成

--- main/api/apis.js
+++ main/api/apis.js
@@ -13,14 +13,34 @@ class Apis {
     this.r_session = "r_session=" + value;
   }
   async getMessage(messageId) {
-    const res = await fetch(this.basepath + "/message/" + messageId, {
+    const res = await fetch(this.basepath + "/messages/" + messageId, {
       headers: {
         Cookie: this.r_session,
       },
     });
     const data = await res.json();
-    console.log(data);
-    return data;
+    const state = res.status;
+    return { state, data };
+  }
+  async postLogin(username, password) {
+    const body = { username: username, password: password };
+    const res = await fetch(this.basepath + "/login", {
+      method: "Post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    console.log(res);
+    return res.status;
+  }
+  async getUser(userId) {
+    const res = await fetch(this.basepath + "/users/" + userId, {
+      headers: {
+        Cookie: this.r_session,
+      },
+    });
+    const data = await res.json();
   }
 }
 
