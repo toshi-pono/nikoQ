1de8636c1adfaed1b998457ddbc42865ad84a5c9
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-01-05 00:12:31 +0900

    login test

--- render.js
+++ render.js
@@ -1,7 +1,12 @@
+import "secret.js";
+import { myname, mypassword } from "./secret";
+
 window.onload = () => {
   initIPC();
   //window.nikoQ.initWebSocket();
   window.nikoQ.doneLoad();
+  // test
+  // window.nikoQ.login(myname, mypassword);
 };
 
 function initIPC() {
@@ -9,9 +14,9 @@ function initIPC() {
     console.log(message);
   });
   window.nikoQ.loginStatus((status) => {
-    console.log(status);
+    console.log("login", status);
   });
   window.nikoQ.logoutStatus((status) => {
-    console.log(status);
+    console.log("logout", status);
   });
 }
