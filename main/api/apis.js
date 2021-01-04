14ece5d454dcc1e59d75dd7ee4ce2ac2653747fa
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-01-04 19:13:29 +0900

    use eletron-fetch

--- main/api/apis.js
+++ main/api/apis.js
@@ -1,13 +1,11 @@
-const { net } = require("electron");
-const EventEmitter = require("events").EventEmitter;
+const fetch = require("electron-fetch").default;
 
 // *** とりあえずテスト用 ***
 const Cookie = require("../../cookie");
 // ***********************
 
 class Apis {
-  constructor(hostname, basepath) {
-    this.hostname = hostname;
+  constructor(basepath) {
     this.basepath = basepath;
     this.r_session = "";
   }
@@ -17,7 +15,7 @@ class Apis {
   getMessage(messageId) {}
 }
 
-const apis = new Apis("q.trap.jp", "/api/v3");
+const apis = new Apis("https://q.trap.jp/api/v3");
 
 // とりあえず
 apis.setSession(Cookie);
