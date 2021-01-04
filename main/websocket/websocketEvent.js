109bbd273ac087234286d5c8ead1b238fa0fc7c5
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-01-04 23:20:18 +0900

    WebsocketEvent.event の作成

--- main/websocket/websocketEvent.js
+++ main/websocket/websocketEvent.js
@@ -1,13 +1,31 @@
 const apis = require("../api/apis");
 
-const websocketEvent = (message) => {
-  switch (message.type) {
-    case value:
-      break;
-
-    default:
-      break;
+class WebsocketEvent {
+  constructor() {
+    this._wc = null;
+    // "all" | "none" | "message"
+    // all: message_created, message_eddit ,user(on,off)line
+    // message: message_created, message_eddit
+    this.streaming = "all";
   }
-};
+  async event(message) {
+    if (this._wc == null) return;
+    switch (message.type) {
+      case "MESSAGE_CREATED":
+        // 新規メッセージ投稿
+        // memo: メッセージの中身・チャンネル名・送信者名を取得→整形してイベント名と内容を返す
+        const { state, data } = await apis.getMessage(message.body.id);
+        this._wc.send("display-message", data);
+        break;
 
-module.exports = websocketEvent;
+      default:
+        this._wc.send("display-message", message);
+        break;
+    }
+  }
+  setWebContents(wc) {
+    this._wc = wc;
+  }
+}
+
+module.exports = WebsocketEvent;
